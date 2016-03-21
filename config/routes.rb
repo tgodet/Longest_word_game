0a8d8ef87bb86a795c881159240778a85024a189
Thibaud Godet <thibaud.godet@gmail.com>
2016-03-21 18:33:27 +0100

    Exercise 2 finnished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+
+  get "game", to: "wordgame#game"
+  get "score", to: "wordgame#score"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
